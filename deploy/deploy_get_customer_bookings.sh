6e3ed3119e460bb2a53491e495005522252706da
ykpfly <yangku@sheridancollege.ca>
2026-01-05 22:57:03 -0500

    feat: update `get_customer_bookings` lambda function and its deployment package.

--- deploy/deploy_get_customer_bookings.sh
+++ deploy/deploy_get_customer_bookings.sh
@@ -2,11 +2,11 @@
 set -e
 
 FUNC_NAME="get_customer_bookings"
-SRC_DIR="../lambda/bookings/get_customer_bookings"
-BUILD_DIR=".build/${FUNC_NAME}"
+SRC_DIR="lambda/bookings/get_customer_bookings"
+BUILD_DIR="deploy/.build/${FUNC_NAME}"
 ZIP_FILE="${FUNC_NAME}.zip"
 AWS_REGION="us-east-2"
-REQ_FILE="../lambda_requirements.txt"
+REQ_FILE="lambda_requirements.txt"
 
 echo "👉 Cleaning build dir..."
 rm -rf "$BUILD_DIR"
@@ -14,7 +14,7 @@ mkdir -p "$BUILD_DIR"
 
 echo "👉 Copying source code..."
 cp "${SRC_DIR}/handler.py" "$BUILD_DIR/"
-cp -r ../src "$BUILD_DIR/"
+cp -r src "$BUILD_DIR/"
 
 echo "👉 Installing dependencies from ${REQ_FILE} ..."
 pip install -r "${REQ_FILE}" -t "$BUILD_DIR" > /dev/null
